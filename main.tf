55d46d4c94083dcdf0e9921f224d5d0aa003938d
Keerti1312 <keertivanalli13@gmail.com>
2023-05-02 20:49:57 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -74,7 +74,8 @@ resource "google_service_account" "gke-sa" {
 resource "google_project_iam_member" "project-iam" {
   project = var.project-id
   role    = "roles/storageobject.viewer"
-  member = "${var.cluster_name}-node-sa@${var.project-id}.iam.gserviceaccount.com"
+  member = "keerti.vanalli@zebra.com"
+  serviceAccount = "${var.cluster_name}-node-sa@${var.project-id}.iam.gserviceaccount.com"
   
 }
 
