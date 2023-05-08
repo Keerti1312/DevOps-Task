22d2e4f67269073e47f9283d2e622d7df03d7af1
Keerti1312 <keertivanalli13@gmail.com>
2023-05-08 11:44:15 +0530

    variables update

--- variables.tf
+++ variables.tf
@@ -3,21 +3,6 @@ variable "project-id"{
     default = "es-devops-sb"
 }
 
-#variable "vpc-name" {
- #   type = string
-  #  default = "task-vpc-network"
-#}
-
-#variable "firewall-name" {
- #   type = string
-  #  default = "task-firewall"
-#}
-
-#variable "subnet-name" {
- #   type = string
-  #  default = "task-subnet"
-#}
-
 variable "subnet-cidr" {
     type = string
     default = "10.20.0.0/24"
