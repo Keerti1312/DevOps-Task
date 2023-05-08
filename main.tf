e159a20eba4eaf17e9447fe2989ce6dd1e934232
Keerti1312 <keertivanalli13@gmail.com>
2023-05-08 11:42:38 +0530

    variables update

--- main.tf
+++ main.tf
@@ -70,12 +70,7 @@ resource "google_container_node_pool" "nodepool_standard" {
 
     // Use the minimal oauth scopes needed
     oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/trace.append",
+    "https://www.googleapis.com/auth/cloud-platform"
     ]
 
     labels = {
